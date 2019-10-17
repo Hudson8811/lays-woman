c131f6e22cc1b752fc364575c743417a02adadc8
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-17 20:49:43 +0600

    desctop

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -29,9 +29,9 @@ $(document).ready(function() {
             $(".recepts [data-filter]").each(function () {
                 var tags = $(this).data('filter');
                 if ($.inArray(filter, tags) > -1) {
-                    $(this).not("[data-filter='all']").removeClass("non-swiper-slide").addClass("swiper-slide");
+                    $(this).removeClass("non-swiper-slide").addClass("swiper-slide");
                 } else {
-                    $(this).not("[data-filter='all']").removeClass("swiper-slide").addClass("non-swiper-slide");
+                    $(this).removeClass("swiper-slide").addClass("non-swiper-slide");
                 }
             });
 
@@ -42,5 +42,100 @@ $(document).ready(function() {
                 mousewheel: true
             });
         }
-    })
+    });
+
+
+    function lockScroll(){
+        $html = $('html');
+        $body = $('body');
+        var initWidth = $body.outerWidth();
+        var initHeight = $body.outerHeight();
+
+        var scrollPosition = [
+            self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+            self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+        ];
+        $html.data('scroll-position', scrollPosition);
+        $html.data('previous-overflow', $html.css('overflow'));
+        $html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+        var marginR = $body.outerWidth()-initWidth;
+        var marginB = $body.outerHeight()-initHeight;
+        $body.css({'margin-right': marginR,'margin-bottom': marginB});
+    }
+
+    function unlockScroll(){
+        $html = $('html');
+        $body = $('body');
+        $html.css('overflow', $html.data('previous-overflow'));
+        var scrollPosition = $html.data('scroll-position');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+
+        $body.css({'margin-right': 0, 'margin-bottom': 0});
+    }
+
+
+    scrolled = 0;
+    scrollLock = 0;
+
+    if ($('.recept-page').hasClass('recept-page')){
+        $(window).on('scroll', function() {
+            var scrollTop = $(this).scrollTop();
+
+            if (scrollTop > 0 && scrolled == 0 && scrollLock == 0){
+                scrollLock = 1;
+                lockScroll();
+                $('.first-screen').slideUp(400);
+                $('html,body').animate({
+                    scrollTop: 1
+                }, 400, 'linear');
+                setTimeout(function () {
+                    scrolled = 1;
+                    scrollLock = 0;
+                    unlockScroll();
+                }, 450);
+            } else if (scrollTop == 0 && scrolled == 1 && scrollLock == 0){
+                scrollLock = 1;
+                $('.first-screen').slideDown(400);
+                lockScroll();
+                setTimeout(function () {
+                    scrolled = 0;
+                    scrollLock = 0;
+                    unlockScroll();
+                }, 450);
+            }
+        });
+
+        $('.first-screen .open .arrow-down').click(function () {
+            if (scrolled == 0 && scrollLock == 0) {
+                scrollLock = 1;
+                lockScroll();
+                $('.first-screen').slideUp(400);
+                $('html,body').animate({
+                    scrollTop: 1
+                }, 400, 'linear');
+                setTimeout(function () {
+                    scrollLock = 0;
+                    scrolled = 1;
+                    unlockScroll();
+                }, 450);
+            }
+        });
+
+
+        var $window = $( window );
+        var $featuredImage = $( ".recept-block .left .img" );
+        var offsetTo = 0;
+        $window.on( "scroll", function() {
+            $featuredImage.toggleClass( "is-sticky",
+                $window.scrollTop() > offsetTo
+            );
+        } );
+    }
+
 });
+
+
+
